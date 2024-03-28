93a12d0ced1b9bbbf2295c4f0576e44eef737426
Edward Koikai <edwardkoikai@gmail.com>
2024-03-29 02:52:31 +0300

    modidifies index.html to add functionality

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++){
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+// }
+// const gifts = ["teddybear", "drone", "doll"]
+// function wrapGifts(gifts){
+//     for(let i=0; i < gifts.length; i++){
+//         console.log(`Wrapped ${gifts[i]} and added a bowl`)
+//         debugger;
+//     }
+//     return gifts
+// }
+// wrapGifts(gifts)
+//
+let myArray = ["Guadalupe", "Ollie", "Ak"];
+let item = "birthday";
+function writeCards(myArray, item) {
+  let newArray = [];
+  for (let i = 0; i < myArray.length; i++) {
+    newArray.push(`Thank you, ${myArray[i]}, for the wonderful ${item} gift!`);
+    debugger;
+  }
+  return newArray;
+}
+
+function countDown(num){
+    
+    while(num >= 0){
+        console.log(num)
+        num--;
+    }
+    return num;
+}
+countDown(10);
